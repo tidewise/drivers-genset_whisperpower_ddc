b1c478fb24f57e5621d52dc82954d89ed6e3e4eb
MateusPedrosa <mateusmacpedrosa@gmail.com>
2021-04-01 08:46:16 -0300

    Fix expected checksum on tests

--- test/test_VariableSpeed.cpp
+++ test/test_VariableSpeed.cpp
@@ -22,18 +22,18 @@ TEST_F(VariableSpeedTest, it_computes_the_checksum) {
 TEST_F(VariableSpeedTest, it_formats_a_frame) {
     uint8_t buffer[16];
     uint8_t payload[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    uint8_t* bufferEnd = variable_speed::formatFrame(buffer, variable_speed::TARGET_ADDRESS, variable_speed::SOURCE_ADDRESS, 0x1, payload, payload + 10);
+    uint8_t* bufferEnd = variable_speed::formatFrame(buffer, variable_speed::TARGET_ADDRESS, variable_speed::SOURCE_ADDRESS, 0x01, payload, payload + 10);
     ASSERT_EQ(16, bufferEnd - buffer);
 
     /** checksum: 
      * variable_speed::TARGET_ADDRESS = 0x0081
      * variable_speed::SOURCE_ADDRESS = 0x0080
-     * command: 0x1
+     * command: 0x01
      * 0+1+2+3+4+5+6+7+8+9 = 45 = 101101b = 0x2D
-     * checksum = LSB(0x81 + 0x00 + 0x80 + 0x00 + 0x1 + 0x2D) = LSB(0x12F) = 0x2F
+     * checksum = LSB(0x81 + 0x00 + 0x80 + 0x00 + 0x01 + 0x2D) = LSB(0x137) = 0x37
      */
     uint8_t expected[] = { variable_speed::TARGET_ADDRESS & 0xFF, (variable_speed::TARGET_ADDRESS >> 8) & 0xFF, variable_speed::SOURCE_ADDRESS & 0xFF,
-                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x2F };
+                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x01, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x37 };
     ASSERT_THAT(std::vector<uint8_t>(buffer, bufferEnd),
                 ElementsAreArray(expected));
 }
@@ -41,17 +41,17 @@ TEST_F(VariableSpeedTest, it_formats_a_frame) {
 TEST_F(VariableSpeedTest, it_throws_if_the_resulting_frame_size_would_be_different_from_16_bytes) {
     uint8_t buffer[0];
     ASSERT_THROW(variable_speed::formatFrame(nullptr, variable_speed::TARGET_ADDRESS,
-                 variable_speed::SOURCE_ADDRESS, 0x1, buffer, buffer + 11),
+                 variable_speed::SOURCE_ADDRESS, 0x01, buffer, buffer + 11),
                  std::invalid_argument);
 }
 
 TEST_F(VariableSpeedTest, it_parses_a_frame) {
     uint8_t bytes[] = { variable_speed::TARGET_ADDRESS & 0xFF, (variable_speed::TARGET_ADDRESS >> 8) & 0xFF, variable_speed::SOURCE_ADDRESS & 0xFF,
-                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x1, 0, 1, 2, 3, 0x2F };
+                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x01, 0, 1, 2, 3, 0x10 };
     Frame frame = variable_speed::parseFrame(bytes, bytes + 10);
     ASSERT_EQ(variable_speed::TARGET_ADDRESS, frame.targetID);
     ASSERT_EQ(variable_speed::SOURCE_ADDRESS, frame.sourceID);
-    ASSERT_EQ(0x1, frame.command);
+    ASSERT_EQ(0x01, frame.command);
 
     uint8_t payload[4] = { 0, 1, 2, 3 };
     ASSERT_THAT(frame.payload, ElementsAreArray(payload));
@@ -64,7 +64,7 @@ TEST_F(VariableSpeedTest, it_throws_if_attempting_to_parse_a_buffer_of_wrong_siz
 
 TEST_F(VariableSpeedTest, it_throws_if_the_checksum_check_fails) {
     uint8_t bytes[] = { variable_speed::TARGET_ADDRESS & 0xFF, (variable_speed::TARGET_ADDRESS >> 8) & 0xFF, variable_speed::SOURCE_ADDRESS & 0xFF,
-                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x1, 0, 1, 2, 3, 0x30 };
+                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x01, 0, 1, 2, 3, 0x11 };
     ASSERT_THROW(variable_speed::parseFrame(bytes, bytes + 10), variable_speed::InvalidChecksum);
 }
 
@@ -75,6 +75,6 @@ TEST_F(VariableSpeedTest, it_throws_if_the_buffer_is_of_wrong_size_when_validati
 
 TEST_F(VariableSpeedTest, it_recognizes_a_valid_checksum) {
     uint8_t frame[] = { variable_speed::TARGET_ADDRESS & 0xFF, (variable_speed::TARGET_ADDRESS >> 8) & 0xFF, variable_speed::SOURCE_ADDRESS & 0xFF,
-                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x1, 0, 1, 2, 3, 0x2F };
+                           (variable_speed::SOURCE_ADDRESS >> 8) & 0xFF, 0x01, 0, 1, 2, 3, 0x10 };
     ASSERT_TRUE(variable_speed::isChecksumValid(frame, frame + 10));
 }
