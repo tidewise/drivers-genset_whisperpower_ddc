739d0cf8ee13b3e3074f54f8cd5e14440ddc9e1f
MateusPedrosa <mateusmacpedrosa@gmail.com>
2021-04-08 15:36:31 -0300

    Implement Main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,283 @@
 #include <iostream>
+#include <list>
+#include <memory>
 #include <genset_whisperpower_ddc/VariableSpeedMaster.hpp>
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace genset_whisperpower_ddc;
+
+struct Command02Data {
+    int rpm;
+    int udcStartBattery;
+    bool overallAlarm;
+    bool engineTempAlarm;
+    bool pmVoltageAlarm;
+    bool oilPressAlarm;
+    bool exhaustTempAlarm;
+    bool uac1Alarm;
+    bool iac1Alarm;
+    bool oilPressHighAlarm;
+    bool lowStartBatteryVoltAlarm;
+    bool startFailure;
+    bool runSignal;
+    bool startByOpUnit;
+    bool mdlDetection50Hz;
+    bool mdlDetection60Hz;
+    bool mdlDetection3Phase;
+    bool mobileMdlDetection;
+    string generatorStatus;
+    int generatorType;
+};
+
+struct Command14Data
+{
+    int totalMinutes;
+    int totalHours;
+    int historicalMinutes;
+    int historicalHours;
+};
+
+
+
+void usage(ostream& stream) {
+    stream << "usage: genset_whisperpower_ddc_ctl URI CMD\n"
+           << "where:\n"
+           << "  URI is a iodrivers_base URI\n"
+           << "\n"
+           << "Available Commands\n"
+           << "  send_control_command CONTROL_COMMAND: send command F7 frame containing a control command\n"
+           << "    CONTROL_COMMAND can be start, stop or keep_alive\n"
+           << "  read_frame [--verbose]\n"
+           << "    --verbose: print the read frames attributes\n"
+           << endl;
+}
 
 int main(int argc, char** argv)
 {
-	genset_whisperpower_ddc::VariableSpeedMaster variableSpeedMaster;
+    list<string> args(argv + 1, argv + argc);
+    if (args.size() < 2) {
+        bool error = !args.empty();
+        usage(error ? cerr : cout);
+        return error;
+    }
+    string uri = args.front();
+    args.pop_front();
 
+    if (args.empty()) {
+        usage(cerr);
+        return 1;
+    }
+
+    string cmd = args.front();
+    args.pop_front();
+
+    std::unique_ptr<genset_whisperpower_ddc::VariableSpeedMaster> genset_whisperpower_ddc_master;
+    auto* master = new genset_whisperpower_ddc::VariableSpeedMaster();
+    genset_whisperpower_ddc_master.reset(master);
+    master->openURI(uri);
+
+    if (cmd == "send_control_command") {
+        if (args.size() != 1) {
+            cerr << "wrong number of arguments\n\n";
+            usage(cerr);
+            return 1;
+        }
+
+        string control_command_arg = args.front();
+        int control_command;
+
+        if (control_command_arg == "start") {
+            control_command = 1;
+        }
+        else if (control_command_arg == "stop") {
+            control_command = 2;
+        }
+        else if (control_command_arg == "keep_alive") {
+            control_command = 3;
+        }
+        else {
+            cerr << "unknown control command\n\n";
+            usage(cerr);
+            return 1;
+        }
+
+        args.pop_front();
+
+        genset_whisperpower_ddc_master->sendCommandF7(
+            control_command
+        );
+    }
+    else if (cmd == "read-frame") {
+        if (args.size() > 1) {
+            cerr << "too many arguments\n\n";
+            usage(cerr);
+            return 1;
+        }
+
+        bool verbose = false;
+        if (args.size() == 1) {
+            if (args.front() == "--verbose") {
+                verbose = true;
+                args.pop_front();
+            }
+            else {
+                cerr << "unknown argument '" << args.front() << "'\n\n";
+                usage(cerr);
+                return 1;
+            }
+        }
+
+        string target;
+        string source;
+
+        cout << "Listening... Press Ctrl+C to stop" << endl;
+        while(1) {
+            Frame frame = genset_whisperpower_ddc_master->readFrame();
+
+            if (frame.targetID == 0x0081) {
+                target = "monitor panels";
+            }
+            else {
+                cerr << "unknown target ID '0x" <<  std::hex << (uint16_t) frame.targetID << "'\n\n";
+                return 1;
+            }
+
+            if (frame.sourceID == 0x0088) {
+                source = "DDC GenVerter power controller";
+            }
+            else {
+                cerr << "unknown source ID '0x" <<  std::hex << (uint16_t) frame.sourceID << "'\n\n";
+                return 1;
+            }
+
+            if (frame.command == 2){
+                Command02Data data;
+                data.rpm = (frame.payload[1] << 8) | frame.payload[0];
+                data.udcStartBattery = (frame.payload[3] << 8) | frame.payload[2];
+                std::bitset<8> statusA(frame.payload[4]);
+                data.overallAlarm = statusA[0];
+                data.engineTempAlarm = statusA[1];
+                data.pmVoltageAlarm = statusA[2];
+                data.oilPressAlarm = statusA[3];
+                data.exhaustTempAlarm = statusA[4];
+                data.uac1Alarm = statusA[5];
+                data.iac1Alarm = statusA[6];
+                data.oilPressHighAlarm = statusA[7];
+                std::bitset<8> statusB(frame.payload[5]);
+                data.lowStartBatteryVoltAlarm = statusB[2];
+                data.startFailure = statusB[4];
+                data.runSignal = statusB[5];
+                data.startByOpUnit = statusB[7];
+                std::bitset<8> statusC(frame.payload[6]);
+                data.mdlDetection50Hz = statusC[2];
+                data.mdlDetection60Hz = statusC[3];
+                data.mdlDetection3Phase = statusC[4];
+                data.mobileMdlDetection = statusC[5];
+                if (frame.payload[7] == 0x00){
+                    data.generatorStatus = "None";
+                }
+                else if (frame.payload[7] == 0x01){
+                    data.generatorStatus = "PreGlow";
+                }
+                else if (frame.payload[7] == 0x02){
+                    data.generatorStatus = "Glow";
+                }
+                else if (frame.payload[7] == 0x03){
+                    data.generatorStatus = "Start";
+                }
+                else if (frame.payload[7] == 0x04){
+                    data.generatorStatus = "Switch";
+                }
+                else if (frame.payload[7] == 0x05){
+                    data.generatorStatus = "Stop";
+                }
+                else if (frame.payload[7] == 0x06){
+                    data.generatorStatus = "Failure";
+                }
+                else if (frame.payload[7] == 0x07){
+                    data.generatorStatus = "Present";
+                }
+                else if (frame.payload[7] == 0x08){
+                    data.generatorStatus = "First";
+                }
+                else if (frame.payload[7] == 0x09){
+                    data.generatorStatus = "ErrorBypass";
+                }
+                else if (frame.payload[7] == 0x0A){
+                    data.generatorStatus = "PreErrorBypass";
+                }
+                else if (frame.payload[7] == 0x0B){
+                    data.generatorStatus = "Pumping";
+                }
+                else if (frame.payload[7] == 0x0C){
+                    data.generatorStatus = "Restart";
+                }
+                else if (frame.payload[7] == 0x0D){
+                    data.generatorStatus = "HydroBoost";
+                }
+                else{
+                    cerr << "unknown generator status '0x" <<  std::hex << (uint16_t) frame.payload[7] << "'\n\n";
+                    return 1;
+                }
+                data.generatorType = frame.payload[8];
+
+                if (verbose) {
+                    cout << "target: " << target << endl;
+                    cout << "source: " << source << endl;
+                    cout << "command: " << (int) frame.command << endl;
+                    cout << "command: 0x" << std::hex << (uint16_t) frame.command << endl;
+                    cout << "RPM: " << data.rpm << endl;
+                    cout << "Udc start battery: " << data.udcStartBattery * 0.01 << "V" << endl;
+                    cout << "overall alarm: " << (data.overallAlarm ? "true" : "false") << endl;
+                    cout << "engine temperature alarm: " << (data.engineTempAlarm ? "true" : "false") << endl;
+                    cout << "PM voltage alarm: " << (data.pmVoltageAlarm ? "true" : "false") << endl;
+                    cout << "oil pressure alarm: " << (data.oilPressAlarm ? "true" : "false") << endl;
+                    cout << "exhaust temperature alarm: " << (data.exhaustTempAlarm ? "true" : "false") << endl;
+                    cout << "Uac1 alarm: " << (data.uac1Alarm ? "true" : "false") << endl;
+                    cout << "Iac1 alarm: " << (data.iac1Alarm ? "true" : "false") << endl;
+                    cout << "oil pressure high alarm: " << (data.oilPressHighAlarm ? "true" : "false") << endl;
+                    cout << "low start battery voltage alarm: " << (data.lowStartBatteryVoltAlarm ? "true" : "false") << endl;
+                    cout << "start failure: " << (data.startFailure ? "true" : "false") << endl;
+                    cout << "run signal: " << (data.runSignal ? "true" : "false") << endl;
+                    cout << "start by operation unit: " << (data.startByOpUnit ? "true" : "false") << endl;
+                    cout << "50 Hz model detection: " << (data.mdlDetection50Hz ? "true" : "false") << endl;
+                    cout << "60 Hz model detection: " << (data.mdlDetection60Hz ? "true" : "false") << endl;
+                    cout << "3 phase model detection: " << (data.mdlDetection3Phase ? "true" : "false") << endl;
+                    cout << "mobile model detection: " << (data.mobileMdlDetection ? "true" : "false") << endl;
+                    cout << "generator status: " << data.generatorStatus << endl;
+                    cout << "generator type: " << data.generatorType << endl;
+                    cout << endl;
+                }
+            }
+            else if (frame.command == 14) {
+                Command14Data data;
+                data.totalMinutes = frame.payload[0];
+                data.totalHours = (frame.payload[3] << 16) | (frame.payload[2] << 8) | frame.payload[1];
+                data.historicalMinutes = frame.payload[4];
+                data.historicalHours = (frame.payload[7] << 16) | (frame.payload[6] << 8) | frame.payload[5];
+
+                if (verbose) {
+                    cout << "target: " << target << endl;
+                    cout << "source: " << source << endl;
+                    cout << "command: " << (int) frame.command << endl;
+                    cout << "command: 0x" << std::hex << (uint16_t) frame.command << endl;
+                    cout << "total run time to be reset after maintenance: " << data.totalHours << "h" << data.totalMinutes << "min" << endl;
+                    cout << "historical run time: " << data.historicalHours << "h" << data.historicalMinutes << "min" << endl;
+                    cout << endl;
+                }
+            }
+            else {
+                cout << "target: " << target << endl;
+                cout << "source: " << source << endl;
+                cout << "command: " << frame.command << endl;
+            }
+        }
+    }
+    else {
+        cerr << "unknown command '" << cmd << "'\n\n";
+        usage(cerr);
+        return 1;
+    }
     return 0;
 }
--- src/VariableSpeed.cpp
+++ src/VariableSpeed.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 using namespace genset_whisperpower_ddc;
 
-uint8_t* variable_speed::formatFrame(uint8_t* buffer, uint16_t targetID, uint16_t sourceID, uint8_t command,
-                     std::vector<uint8_t> const& payload) {
+uint8_t *variable_speed::formatFrame(uint8_t *buffer, uint16_t targetID, uint16_t sourceID, uint8_t command,
+                                     std::vector<uint8_t> const &payload)
+{
 
     auto begin = &payload[0];
     return variable_speed::formatFrame(buffer, targetID, sourceID, command, begin, begin + payload.size());
 }
-uint8_t* variable_speed::formatFrame(uint8_t* buffer, uint16_t targetID, uint16_t sourceID, uint8_t command,
-                                uint8_t const* payloadStart, uint8_t const* payloadEnd) {
+uint8_t *variable_speed::formatFrame(uint8_t *buffer, uint16_t targetID, uint16_t sourceID, uint8_t command,
+                                     uint8_t const *payloadStart, uint8_t const *payloadEnd)
+{
 
     int payloadSize = payloadEnd - payloadStart;
-    if (payloadSize + FRAME_OVERHEAD_SIZE != SENT_FRAME_SIZE) {
-        throw std::invalid_argument("variable_speed::formatFrame: frame size would be different "\
+    if (payloadSize + FRAME_OVERHEAD_SIZE != SENT_FRAME_SIZE)
+    {
+        throw std::invalid_argument("variable_speed::formatFrame: frame size would be different "
                                     "from specified size of 10");
     }
 
@@ -31,26 +34,32 @@ uint8_t* variable_speed::formatFrame(uint8_t* buffer, uint16_t targetID, uint16_
     return bufferPayload + payloadSize + 1;
 }
 
-static void validateBufferSize(uint8_t const* start, uint8_t const* end,
-                               char const* context) {
-    if (end - start != variable_speed::RECEIVED_FRAME_SIZE) {
+static void validateBufferSize(uint8_t const *start, uint8_t const *end,
+                               char const *context)
+{
+    if (end - start != variable_speed::RECEIVED_FRAME_SIZE)
+    {
         throw variable_speed::WrongSize(
             string(context) + ": "
-            "expected " + to_string(variable_speed::RECEIVED_FRAME_SIZE) + " bytes, "
-            "but got " + to_string(end - start)
-        );
+                              "expected " +
+            to_string(variable_speed::RECEIVED_FRAME_SIZE) + " bytes, "
+                                                             "but got " +
+            to_string(end - start));
     }
 }
 
-Frame variable_speed::parseFrame(uint8_t const* start, uint8_t const* end) {
+Frame variable_speed::parseFrame(uint8_t const *start, uint8_t const *end)
+{
     Frame result;
     parseFrame(result, start, end);
     return result;
 }
 
-void variable_speed::parseFrame(Frame& frame, uint8_t const* start, uint8_t const* end) {
+void variable_speed::parseFrame(Frame &frame, uint8_t const *start, uint8_t const *end)
+{
     validateBufferSize(start, end, "variable_speed::parseFrame");
-    if (!isChecksumValid(start, end)) {
+    if (!isChecksumValid(start, end))
+    {
         throw InvalidChecksum("variable_speed::parseFrame: checksum failed");
     }
 
@@ -65,22 +74,25 @@ void variable_speed::parseFrame(Frame& frame, uint8_t const* start, uint8_t cons
     std::copy(start + FRAME_HEADER_SIZE, end - 1, frame.payload.begin());
 }
 
-
-bool variable_speed::isChecksumValid(uint8_t const* start, uint8_t const* end) {
+bool variable_speed::isChecksumValid(uint8_t const *start, uint8_t const *end)
+{
     validateBufferSize(start, end, "variable_speed::isChecksumValid");
     uint8_t expected = checksum(start, end - 1);
     return (end[-1] == expected);
 }
 
-uint8_t variable_speed::checksum(uint8_t const* start, uint8_t const* end) {
+uint8_t variable_speed::checksum(uint8_t const *start, uint8_t const *end)
+{
     uint16_t checksum = 0;
-    for (uint8_t const* it = start; it != end; ++it) {
-        checksum += (uint16_t) *it;
+    for (uint8_t const *it = start; it != end; ++it)
+    {
+        checksum += (uint16_t)*it;
     }
     return (checksum & 0xFF); // get lowest byte
 }
 
-std::vector<uint8_t> variable_speed::formatCommandF7Data(uint8_t controlCommand) {
+std::vector<uint8_t> variable_speed::formatCommandF7Data(uint8_t controlCommand)
+{
     std::vector<uint8_t> payload;
     payload.push_back(controlCommand);
     payload.push_back(0x00); // byte not used
