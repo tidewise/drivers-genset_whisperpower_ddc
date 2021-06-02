d5ceef8b40624c6bd0d952c030b5a8cbeae6ed65
MateusPedrosa <mateus.pedrosa@tidewise.io>
2021-06-02 17:43:08 -0300

    Change implementation
    - Must read a valid frame before sending a command
    - Change "send_control_command" to "run", so that it keeps sending the
    keep_alive command until the user tells it to stop by pressing Ctrl+C.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <memory>
+#include <signal.h>
 #include <genset_whisperpower_ddc/VariableSpeed.hpp>
 #include <genset_whisperpower_ddc/VariableSpeedMaster.hpp>
 #include <genset_whisperpower_ddc/ControlCommand.hpp>
@@ -8,20 +9,56 @@
 using namespace std;
 using namespace genset_whisperpower_ddc;
 
+bool stop = false;
+
 void usage(ostream& stream) {
     stream << "usage: genset_whisperpower_ddc_ctl URI CMD\n"
            << "where:\n"
-           << "  URI is a iodrivers_base URI\n"
+           << "    URI is a iodrivers_base URI\n"
            << "\n"
            << "Available Commands\n"
-           << "  send_control_command CONTROL_COMMAND: send a control command to the DDC GenVerter power controller\n"
-           << "    CONTROL_COMMAND can be start, stop or keep_alive\n"
-           << "  read_frame: read a frame and print its content\n"
+           << "    run: send start control command to the DDC GenVerter power controller and then keep it running by "
+           << "repeatedly sending the keep_alive command until the user press any key. When this happens, send the stop command to stop de generator.\n"
+           << "    read_frame: read a frame and print its content\n"
            << endl;
 }
 
-int main(int argc, char** argv)
-{
+void handler(int s) {
+    stop = true;
+}
+
+void getValidFrame(std::unique_ptr<VariableSpeedMaster> &master) {
+    Frame frame;
+    bool validFrame;
+    bool receivedGeneratorState = false;
+    bool receivedRunTimeState = false;
+    while (!(receivedGeneratorState || receivedRunTimeState)) {
+        try {
+            frame = master->readFrame();
+            validFrame = true;
+        }
+        catch(const variable_speed::WrongSize& e) {
+            validFrame = false;
+        }
+        catch(const variable_speed::InvalidChecksum& e) {
+            validFrame = false;
+        }
+        catch(const iodrivers_base::UnixError& e) {} // iodrivers_base may throw this error when receiving a SIGINT, but it can be ignored
+
+        if (validFrame) {
+            if (frame.targetID == variable_speed::PANELS_ADDRESS && frame.sourceID == variable_speed::DDC_CONTROLLER_ADDRESS) {
+                if (frame.command == variable_speed::PACKET_GENERATOR_STATE_AND_MODEL){
+                    receivedGeneratorState = true;
+                }
+                else if (frame.command == variable_speed::PACKET_RUN_TIME_STATE) {
+                    receivedRunTimeState = true;
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
     list<string> args(argv + 1, argv + argc);
     if (args.size() < 2) {
         bool error = !args.empty();
@@ -42,36 +79,32 @@ int main(int argc, char** argv)
     std::unique_ptr<VariableSpeedMaster> master(new VariableSpeedMaster());
     master->openURI(uri);
 
-    if (cmd == "send_control_command") {
-        if (args.size() != 1) {
-            cerr << "wrong number of arguments\n\n";
+    if (cmd == "run") {
+        if (args.size() != 0) {
+            cerr << "command run does not accept arguments\n\n";
             usage(cerr);
             return 1;
         }
 
-        string controlCommandArg = args.front();
-        ControlCommand controlCommand;
+        getValidFrame(master);
 
-        if (controlCommandArg == "start") {
-            controlCommand = CONTROL_CMD_START;
-        }
-        else if (controlCommandArg == "stop") {
-            controlCommand = CONTROL_CMD_STOP;
-        }
-        else if (controlCommandArg == "keep_alive") {
-            controlCommand = CONTROL_CMD_KEEP_ALIVE;
-        }
-        else {
-            cerr << "unknown control command\n\n";
-            usage(cerr);
-            return 1;
+        master->sendControlCommand(CONTROL_CMD_START);
+
+        signal(SIGINT, handler);
+
+        // Send keep_alive until user presses Ctrl+C
+        while(!stop) {
+            getValidFrame(master);
+
+            try {
+                master->sendControlCommand(CONTROL_CMD_KEEP_ALIVE);
+            }
+            catch(const iodrivers_base::UnixError& e) {} // iodrivers_base may throw this error when receiving a SIGINT, but it can be ignored
         }
 
-        args.pop_front();
+        getValidFrame(master);
 
-        master->sendControlCommand(
-            controlCommand
-        );
+        master->sendControlCommand(CONTROL_CMD_STOP);
     }
     else if (cmd == "read_frame") {
         if (args.size() != 0) {
@@ -80,50 +113,43 @@ int main(int argc, char** argv)
                 return 1;
         }
 
-        auto now = base::Time::now();
+        base::Time now;
 
         Frame frame;
-        try
-        {
-            frame = master->readFrame();
-        }
-        catch(const variable_speed::WrongSize& e)
-        {
-            cerr << e.what() << "\n";
-            return 1;
-        }
-        catch(const variable_speed::InvalidChecksum& e)
-        {
-            cerr << e.what() << "\n";
-            return 1;
-        }
-
-        if (frame.targetID != variable_speed::TARGET_ADDRESS) {
-            cerr << "unknown target ID '0x" <<  std::hex << (uint16_t) frame.targetID << "'\n\n";
-            return 1;
-        }
-
-        if (frame.sourceID != variable_speed::SOURCE_ADDRESS) {
-            cerr << "unknown source ID '0x" <<  std::hex << (uint16_t) frame.sourceID << "'\n\n";
-            return 1;
-        }
-
-        if (frame.command == variable_speed::PACKET_GENERATOR_STATE_AND_MODEL){
-            std::pair<GeneratorState, GeneratorModel> generatorStateAndModel = master->parseGeneratorStateAndModel(frame.payload, now);
-
-            cout << generatorStateAndModel.first << endl;
-            cout << generatorStateAndModel.second << endl;
-        }
-        else if (frame.command == variable_speed::PACKET_RUN_TIME_STATE) {
-            RunTimeState runTimeState = master->parseRunTimeState(frame.payload, now);
-
-            cout << runTimeState << endl;
-        }
-        else {
-            cerr << "unknown command '0x" <<  std::hex << (uint16_t) frame.command << "'\n\n";
-            return 1;
+        bool validFrame;
+        std::pair<GeneratorState, GeneratorModel> generatorStateAndModel;
+        RunTimeState runTimeState;
+        bool receivedGeneratorState = false;
+        bool receivedRunTimeState = false;
+        while (!(receivedGeneratorState && receivedRunTimeState)) {
+            try {
+                frame = master->readFrame();
+                validFrame = true;
+            }
+            catch(const variable_speed::WrongSize& e) {
+                validFrame = false;
+            }
+            catch(const variable_speed::InvalidChecksum& e) {
+                validFrame = false;
+            }
+            if (validFrame) {
+                if (frame.targetID == variable_speed::PANELS_ADDRESS && frame.sourceID == variable_speed::DDC_CONTROLLER_ADDRESS) {
+                    now = base::Time::now();
+                    if (frame.command == variable_speed::PACKET_GENERATOR_STATE_AND_MODEL){
+                        generatorStateAndModel = master->parseGeneratorStateAndModel(frame.payload, now);
+                        receivedGeneratorState = true;
+                    }
+                    else if (frame.command == variable_speed::PACKET_RUN_TIME_STATE) {
+                        runTimeState = master->parseRunTimeState(frame.payload, now);
+                        receivedRunTimeState = true;
+                    }
+                }
+            }
         }
 
+        cout << generatorStateAndModel.first << endl;
+        cout << generatorStateAndModel.second << endl;
+        cout << runTimeState << endl;
     }
     else {
         cerr << "unknown command '" << cmd << "'\n\n";
