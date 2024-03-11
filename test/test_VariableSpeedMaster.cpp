da31f9649346aefa09ffd88a3e92fc60912aaebb
Rayssa Soares <rayssa.soares@tidewise.io>
2024-03-11 17:55:19 -0300

    fix: do not report start_signals as an alarm

--- test/test_VariableSpeedMaster.cpp
+++ test/test_VariableSpeedMaster.cpp
@@ -1,27 +1,31 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include <genset_whisperpower_ddc/VariableSpeedMaster.hpp>
-#include <genset_whisperpower_ddc/VariableSpeed.hpp>
-#include <iodrivers_base/FixtureGTest.hpp>
 #include <fcntl.h>
+#include <genset_whisperpower_ddc/VariableSpeed.hpp>
+#include <genset_whisperpower_ddc/VariableSpeedMaster.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <iodrivers_base/FixtureGTest.hpp>
 #include <thread>
 
 #define PI 3.141592653589793
 
 using namespace std;
 using namespace genset_whisperpower_ddc;
-using testing::ElementsAreArray;
 using base::Time;
+using testing::ElementsAreArray;
 
-struct VariableSpeedMasterTest : public ::testing::Test, iodrivers_base::Fixture<VariableSpeedMaster> {
-    VariableSpeedMasterTest() {
+struct VariableSpeedMasterTest : public ::testing::Test,
+                                 iodrivers_base::Fixture<VariableSpeedMaster> {
+    VariableSpeedMasterTest()
+    {
     }
 
-    ~VariableSpeedMasterTest() {
+    ~VariableSpeedMasterTest()
+    {
     }
 };
 
-TEST_F(VariableSpeedMasterTest, it_throws_if_calling_readPacket) {
+TEST_F(VariableSpeedMasterTest, it_throws_if_calling_readPacket)
+{
     driver.openURI("test://");
     // push one byte to get into extractPacket
     uint8_t buffer[1];
@@ -29,49 +33,97 @@ TEST_F(VariableSpeedMasterTest, it_throws_if_calling_readPacket) {
     ASSERT_THROW(driver.readPacket(buffer, 1024), std::logic_error);
 }
 
-TEST_F(VariableSpeedMasterTest, it_reads_a_frame) {
+TEST_F(VariableSpeedMasterTest, it_reads_a_frame)
+{
     driver.openURI("test://");
 
-    std::vector<uint8_t> buffer = { variable_speed::PANELS_ADDRESS & 0xFF, (variable_speed::PANELS_ADDRESS >> 8) & 0xFF, variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
-                           (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF, variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x38 };
+    std::vector<uint8_t> buffer = {variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_GENERATOR_STATE_AND_MODEL,
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        0x38};
     pushDataToDriver(&buffer[0], &buffer[16]);
 
     Frame frame = driver.readFrame();
     ASSERT_EQ(variable_speed::PANELS_ADDRESS, frame.targetID);
     ASSERT_EQ(variable_speed::DDC_CONTROLLER_ADDRESS, frame.sourceID);
     ASSERT_EQ(variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, frame.command);
-    uint8_t payload[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    uint8_t payload[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     ASSERT_THAT(frame.payload, ElementsAreArray(payload));
 }
 
-TEST_F(VariableSpeedMasterTest, it_writes_a_frame) {
+TEST_F(VariableSpeedMasterTest, it_writes_a_frame)
+{
     driver.openURI("test://");
 
-    driver.writeFrame(variable_speed::PACKET_START_STOP, std::vector<uint8_t> {0, 1, 2, 3});
+    driver.writeFrame(variable_speed::PACKET_START_STOP,
+        std::vector<uint8_t>{0, 1, 2, 3});
     auto bytes = readDataFromDriver();
 
-    uint8_t expected[] = { variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF, (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF, variable_speed::PANELS_ADDRESS & 0xFF,
-                           (variable_speed::PANELS_ADDRESS >> 8) & 0xFF, variable_speed::PACKET_START_STOP, 0x00, 0x01, 0x02, 0x03, 0x06 };
+    uint8_t expected[] = {variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_START_STOP,
+        0x00,
+        0x01,
+        0x02,
+        0x03,
+        0x06};
     ASSERT_THAT(bytes, ElementsAreArray(expected));
 }
 
-
-TEST_F(VariableSpeedMasterTest, it_sends_control_command) {
+TEST_F(VariableSpeedMasterTest, it_sends_control_command)
+{
     driver.openURI("test://");
 
     driver.sendControlCommand(variable_speed::PACKET_GENERATOR_STATE_AND_MODEL);
     auto bytes = readDataFromDriver();
 
-    uint8_t expected[] = { variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF, (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF, variable_speed::PANELS_ADDRESS & 0xFF,
-                           (variable_speed::PANELS_ADDRESS >> 8) & 0xFF, variable_speed::PACKET_START_STOP, variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, 0x00, 0x00, 0x00, 0x02 };
-     ASSERT_THAT(bytes, ElementsAreArray(expected));
+    uint8_t expected[] = {variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_START_STOP,
+        variable_speed::PACKET_GENERATOR_STATE_AND_MODEL,
+        0x00,
+        0x00,
+        0x00,
+        0x02};
+    ASSERT_THAT(bytes, ElementsAreArray(expected));
 }
 
-TEST_F(VariableSpeedMasterTest, it_parses_generator_state_and_model) {
+TEST_F(VariableSpeedMasterTest, it_parses_generator_state_and_model)
+{
     driver.openURI("test://");
 
-    std::vector<uint8_t> buffer = { variable_speed::PANELS_ADDRESS & 0xFF, (variable_speed::PANELS_ADDRESS >> 8) & 0xFF, variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
-                           (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF, variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x38 };
+    std::vector<uint8_t> buffer = {variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_GENERATOR_STATE_AND_MODEL,
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        0x38};
     pushDataToDriver(&buffer[0], &buffer[16]);
 
     Frame frame = driver.readFrame();
@@ -79,11 +131,12 @@ TEST_F(VariableSpeedMasterTest, it_parses_generator_state_and_model) {
     ASSERT_EQ(variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, frame.command);
 
     Time now = Time::now();
-    std::pair<GeneratorState, GeneratorModel> generatorStateAndModel = driver.parseGeneratorStateAndModel(frame.payload, now);
+    std::pair<GeneratorState, GeneratorModel> generatorStateAndModel =
+        driver.parseGeneratorStateAndModel(frame.payload, now);
 
     GeneratorState expectedState;
     expectedState.time = now;
-    expectedState.rotation_speed = ((2*PI)/60) * 0x0100;
+    expectedState.rotation_speed = ((2 * PI) / 60) * 0x0100;
     expectedState.start_battery_voltage = 0.01 * 0x0302;
     expectedState.alarms = 0x0504;
     expectedState.start_signals = 0x05;
@@ -95,20 +148,94 @@ TEST_F(VariableSpeedMasterTest, it_parses_generator_state_and_model) {
 
     ASSERT_EQ(generatorStateAndModel.first.time, expectedState.time);
     ASSERT_EQ(generatorStateAndModel.first.rotation_speed, expectedState.rotation_speed);
-    ASSERT_EQ(generatorStateAndModel.first.start_battery_voltage, expectedState.start_battery_voltage);
+    ASSERT_EQ(generatorStateAndModel.first.start_battery_voltage,
+        expectedState.start_battery_voltage);
     ASSERT_EQ(generatorStateAndModel.first.alarms, expectedState.alarms);
     ASSERT_EQ(generatorStateAndModel.first.start_signals, expectedState.start_signals);
-    ASSERT_EQ(generatorStateAndModel.first.generator_status, expectedState.generator_status);
+    ASSERT_EQ(generatorStateAndModel.first.generator_status,
+        expectedState.generator_status);
 
-    ASSERT_EQ(generatorStateAndModel.second.model_detection, expectedModel.model_detection);
+    ASSERT_EQ(generatorStateAndModel.second.model_detection,
+        expectedModel.model_detection);
     ASSERT_EQ(generatorStateAndModel.second.generator_type, expectedModel.generator_type);
 }
 
-TEST_F(VariableSpeedMasterTest, it_parses_run_time_state) {
+TEST_F(VariableSpeedMasterTest, it_does_not_fill_alarms_bits_with_start_signals_bits)
+{
     driver.openURI("test://");
 
-    std::vector<uint8_t> buffer = { variable_speed::PANELS_ADDRESS & 0xFF, (variable_speed::PANELS_ADDRESS >> 8) & 0xFF, variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
-                           (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF, variable_speed::PACKET_RUN_TIME_STATE, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0x44 };
+    std::vector<uint8_t> buffer = {variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_GENERATOR_STATE_AND_MODEL,
+        0,
+        1,
+        2,
+        3,
+        4,
+        32,
+        6,
+        7,
+        8,
+        9,
+        83};
+    pushDataToDriver(&buffer[0], &buffer[16]);
+
+    Frame frame = driver.readFrame();
+
+    ASSERT_EQ(variable_speed::PACKET_GENERATOR_STATE_AND_MODEL, frame.command);
+
+    Time now = Time::now();
+    std::pair<GeneratorState, GeneratorModel> generatorStateAndModel =
+        driver.parseGeneratorStateAndModel(frame.payload, now);
+
+    GeneratorState expectedState;
+    expectedState.time = now;
+    expectedState.rotation_speed = ((2 * PI) / 60) * 0x0100;
+    expectedState.start_battery_voltage = 0.01 * 0x0302;
+    expectedState.alarms = 0x04;
+    expectedState.start_signals = 0x20;
+    expectedState.generator_status = GeneratorStatus::STATUS_PRESENT;
+
+    GeneratorModel expectedModel;
+    expectedModel.model_detection = 0x06;
+    expectedModel.generator_type = 0x08;
+
+    ASSERT_EQ(generatorStateAndModel.first.time, expectedState.time);
+    ASSERT_EQ(generatorStateAndModel.first.rotation_speed, expectedState.rotation_speed);
+    ASSERT_EQ(generatorStateAndModel.first.start_battery_voltage,
+        expectedState.start_battery_voltage);
+    ASSERT_EQ(generatorStateAndModel.first.alarms, expectedState.alarms);
+    ASSERT_EQ(generatorStateAndModel.first.start_signals, expectedState.start_signals);
+    ASSERT_EQ(generatorStateAndModel.first.generator_status,
+        expectedState.generator_status);
+
+    ASSERT_EQ(generatorStateAndModel.second.model_detection,
+        expectedModel.model_detection);
+    ASSERT_EQ(generatorStateAndModel.second.generator_type, expectedModel.generator_type);
+}
+
+TEST_F(VariableSpeedMasterTest, it_parses_run_time_state)
+{
+    driver.openURI("test://");
+
+    std::vector<uint8_t> buffer = {variable_speed::PANELS_ADDRESS & 0xFF,
+        (variable_speed::PANELS_ADDRESS >> 8) & 0xFF,
+        variable_speed::DDC_CONTROLLER_ADDRESS & 0xFF,
+        (variable_speed::DDC_CONTROLLER_ADDRESS >> 8) & 0xFF,
+        variable_speed::PACKET_RUN_TIME_STATE,
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        0x44};
     pushDataToDriver(&buffer[0], &buffer[16]);
 
     Frame frame = driver.readFrame();
@@ -121,7 +248,8 @@ TEST_F(VariableSpeedMasterTest, it_parses_run_time_state) {
     RunTimeState expectedState;
     expectedState.time = now;
     expectedState.total_run_time = Time::fromSeconds((0x030201 * 60 * 60) + (0x00 * 60));
-    expectedState.historical_run_time =  Time::fromSeconds((0x070605 * 60 * 60) + (0x04 * 60));
+    expectedState.historical_run_time =
+        Time::fromSeconds((0x070605 * 60 * 60) + (0x04 * 60));
 
     ASSERT_EQ(runTimeState.time, expectedState.time);
     ASSERT_EQ(runTimeState.total_run_time, expectedState.total_run_time);
