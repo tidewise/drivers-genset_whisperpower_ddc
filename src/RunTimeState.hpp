6e70a121dfff660aac752aa8ad29d0890cb49a88
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-04-28 15:18:26 -0300

    fix: revert alignment between this RunTimeState type and the new one
    The field names of this types are really too confusing. This is the opportunity
    to fix that.

--- src/RunTimeState.hpp
+++ src/RunTimeState.hpp
@@ -1,11 +1,17 @@
 #ifndef GENSET_WHISPERPOWER_DDC_RUN_TIME_STATE_HPP
 #define GENSET_WHISPERPOWER_DDC_RUN_TIME_STATE_HPP
 
-#include <power_whisperpower/RunTimeState.hpp>
+#include <base/Time.hpp>
 
 namespace genset_whisperpower_ddc {
-    struct RunTimeState : public power_whisperpower::RunTimeState {
+    struct RunTimeState {
+        base::Time time;
+        
+        base::Time total_run_time; // Total run time to be reset after maintenance
+        base::Time historical_run_time;
     };
+
+    std::ostream& operator << (std::ostream& io, RunTimeState const& state);
 }
 
 #endif
