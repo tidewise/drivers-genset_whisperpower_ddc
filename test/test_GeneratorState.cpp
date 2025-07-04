d84b439cc67ae414b0612337445602d1f52ebb0f
Jhonas <jhonas.moura@tidewise.io>
2025-07-04 19:08:56 -0300

    fix: map status failure to stopped
    Status failure actually means that the generator is stopped.

--- test/test_GeneratorState.cpp
+++ test/test_GeneratorState.cpp
@@ -17,6 +17,19 @@ TEST_F(GeneratorStateTest, it_converts_to_the_common_GensetState_type)
     ASSERT_EQ(power_whisperpower::GensetState::GENSET_STAGE_STARTING, converted.stage);
 }
 
+TEST_F(GeneratorStateTest, it_converts_genset_failure_to_a_stopped_common_stage)
+{
+    GeneratorState state;
+    state.time = base::Time::fromSeconds(10);
+    state.alarms = 10;
+    state.generator_status = STATUS_FAILURE;
+
+    auto converted = state.toGensetState();
+    ASSERT_EQ(state.time, converted.time);
+    ASSERT_TRUE(converted.failure_detected);
+    ASSERT_EQ(power_whisperpower::GensetState::GENSET_STAGE_STOPPED, converted.stage);
+}
+
 TEST_F(GeneratorStateTest, it_fills_the_failure_detected_field)
 {
     GeneratorState state;
